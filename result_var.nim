1b15725f42e5111f200fe4aca32aeee6294da8a5
Satomin2864 <satomiyuta0602@gmail.com>
2020-01-31 00:59:37 +0900

    返り値を実装した関数

---
+++ result_var.nim
@@ -0,0 +1,13 @@
+# int型のリスト？を引数として受け取って、
+# リストの合計値をint型で返す関数
+proc sumTillNegative(x: varargs[int]): int =
+    # list
+    for i in x:
+        echo i
+        if i < 0:
+            return
+        result = result + i
+
+echo sumTillNegative()
+echo sumTillNegative(3,4,5)
+echo sumTillNegative(3,4,-1,6)
